55350e80562ca7e0ab3f4778983a6aeacb9c7385
Bjunkz <b.eriksson91@gmail.com>
2021-02-04 10:45:28 +0100

    adds goat and levels and styling

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ const description = document.getElementById("description");
 const time = document.getElementById("time");
 const sheep = document.querySelector("#caughtSheep")
 const sheepImg = "<img src='little-sheep.png'>"
+const stopBtn = document.querySelector("#stopBtn")
+
 
 let level=2000
 let timer;
 let sheepCatcher;
 let currentScore = 0;
+let timeLeft = 40; 
+time.innerHTML = "Tid kvar: " + timeLeft;
+
+// STOPPA ELLER STARTA OM SPELET
+stopBtn.addEventListener('click', function(){
+    clearInterval(timer);
+    clearInterval(sheepCatcher);
+    clearAnimal("sheep")
+    clearAnimal("goat")
+    score.innerHTML= "Fångade får: 0";
+    timeLeft = 40
+    time.innerHTML = "Tid kvar: " + timeLeft;
+    currentScore = 0;
+})
 
 //STARTAR SPELET OCH TIMERN
 function startGame() {
@@ -24,7 +40,6 @@ function startGame() {
 
     sheep.innerHTML= ""
 
-    let timeLeft = 20; 
     timer = setInterval(function () {
         timeLeft --
 
@@ -51,16 +66,36 @@ function startGame() {
 
 // PLACERAR ETT FÅR I EN RANDOM BOX 
 function placeSheep(){
-    box.forEach((item, index) => item.classList.remove('sheep'))
+    clearAnimal("sheep")
     let number = Math.floor(getRandomArbitrary(1, 9))
     let div = document.getElementById(number)
     div.classList.add('sheep')
+    placeGoat()
 };
 
+// PLACERAR EN GET SOM GER MINUSPOÄNG OM MAN KLICKAR
+function placeGoat (){
+    clearAnimal("goat")
+    let goatNumber = Math.random()
+    let number = Math.floor(getRandomArbitrary(1, 9))
+    let div = document.getElementById(number)
+    console.log(goatNumber);
+    if(goatNumber<0.9){
+        div.classList.add('goat')
+
+    }
+
+}
 //GENERERAR EN SLUPMÄSSIG SIFFRA MELLAN MIN OCH MAX
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 };
+
+
+// TAR BORT ANIMAL
+function clearAnimal(animal){
+    box.forEach((item, index) => item.classList.remove(animal))
+}
 
  //RÄKNAR FÅREN NÄR MAN KLICKAR PÅ DEM OCH STOPPAR IN FÅREN I HAGEN
 gameBoard.addEventListener('click', function (e) {
@@ -78,10 +113,34 @@ gameBoard.addEventListener('click', function (e) {
         console.log("currentScore", currentScore);
 
         
-        if (currentScore === 3) {
-            alert("Grattis, du har klarat första nivån");
-            level-=80;
+        if (currentScore === 5) {
+            clearInterval(timer);
+            clearInterval(sheepCatcher);
+            alert("Grattis, du har klarat första nivån. Nu går det snabbare");
+            level = 1300;
         };
+
+        if (currentScore === 10) {
+            clearInterval(timer);
+            clearInterval(sheepCatcher);
+            alert("Grattis, du har klarat andra nivån. Nu springer fåren ännu snabbare");
+            level = 800;
+        };
+
+    };
+
+    if (e.target.classList.contains('goat')) {
+        currentScore--;
+        score.innerHTML = "Fångade får: " + currentScore
+        e.target.classList.remove('goat')
+        e.target.classList.add('failGoat')
+        let failGoat = setTimeout(function(){
+            console.log("timeout funkar");
+            e.target.classList.remove('failGoat')
+        }, 200)
+
+        
+        
     };
 
 });
